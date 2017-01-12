da9d73b06279e611dd22b59647b39f4652d49277
Michael Enudi <okmich2002@yahoo.com>
2017-01-11 19:27:10 -0800

    restructing final project

--- final_project/08_01_17/sql/database_obj_preparation.sql
+++
@@ -1,9 +0,0 @@
-create database if not exist handson_class;
-
-use handson_class;
-
-create table  twitter_job (
-	id bigint primary key, ts bigint, twtLang varchar(5), created_at varchar(50),
-	tweetText varchar(255), hashtags varchar(255), source varchar(255), agentLocation varchar(100),
-	agentDescription varchar(200), agentName varchar(100), agentImageUrl varchar(100)
-);
