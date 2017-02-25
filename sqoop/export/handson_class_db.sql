cdfa442f37f423b10e50c5c5f6f5746a36b4ed67
Michael Enudi <okmich2002@yahoo.com>
2017-02-25 11:26:07 -0800

    after flume class

--- sqoop/export/handson_class_db.sql
+++ sqoop/export/handson_class_db.sql
@@ -1,4 +1,7 @@
 -- create a database schema with an empty table
-create database handson_class;
-use handson_class;
+create database sqoop_handson;
+use sqoop_handson;
 create table nyse_dividends (exchange varchar(20), stock_symbol varchar(5), datestring varchar(20), value float(10,2));
+
+
+create table altered_nyse_dividends (stock_symbol varchar(5), value float(10,2), datestring varchar(20), exchange varchar(20));
